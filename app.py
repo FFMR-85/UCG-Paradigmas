1087867e2989990c69d903e6ba4f9c908348d6d7
FFMR-85 <francisco.miranda@cu.ucsg.edu.ec>
2026-09-19 11:29:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import libreria_funciones as lf
 
 st.title("Paradigmas de la programación")
+
+st.sidebar.image("ucsg.png")
 
 st.sidebar.title("Parametros")
 
 st.write("Elaborado por Francisco Miranda")
+
+capital = st.number_input("Ingrese el capital")
+tasa_anual_pct = st.number_input("Ingrese la tasa anual")
+dias_mora = st.number_input("Ingrese dias mora")
+
+# resultado = lf.calcular_interes_mora()
